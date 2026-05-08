88e219034cb2113ecc2c30e5c4a9b44e5fa16c8b
Isha <ishaagrawal281@gmail.com>
2026-05-08 14:49:18 +0530

    Completed ShopKart Routing Assignment

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ import Home from "./pages/Home";
 // TODO: Import Contact
 // TODO: Import React Router
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
       
       {/* TODO: Implement Routing Here */}
       
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,27 @@
 import React from "react";
 
+
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav>
-      {/* TODO: Replace <a> with Link */}
-      
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
-      
+      <ul style={{ display: "flex", gap: "20px", listStyle: "none" }}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
+
 
 export default Navbar;
